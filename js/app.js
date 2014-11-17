b980640cf8abe19c9824d301924190d4a244bfe4
Alyssa <alyssa@thepalesnail.com>
2014-11-17 15:31:57 -0500

    starte dover

--- js/app.js
+++ js/app.js
@@ -1,61 +1,18 @@
-App = Ember.Application.create(); // creating the object App. Roots of the app
+App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.resource('recipes');
-  this.resource('recipe', { path:'recipes/:name' });
-  this.route('edit_recipe', { path:'recipes/:id/edit' });
-
+  // put your routes here
 });
-
-var RECIPES = [{ name: 'banana',
-                 id: 1,
-                 description: 'description',
-                 imgUrl: 'imgUrl',
-                 ingredients:['apple'],
-                 steps: ['do something']
-               }, {
-                 name: 'apple',
-                 id: 2,
-                 description: 'description',
-                 imgUrl: 'imgUrl',
-                 ingredients:['apple'],
-                 steps: ['do something']
-               }]
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return RECIPES;
-      }
-});
-
-App.RecipesRoute = Ember.Route.extend({
-  model:  function(params) {
-   return RECIPES.find(function(recipes){
-     return recipes.name === Number(params.name);
-   })
- }
-});
-
-App.EditRecipeRoute = Ember.Route.extend({
-  model: function(params) {
-    return RECIPES.find(function(recipes){
-      return recipes.name === Number(params.name);
-    })
   }
 });
 
-App.RecipesRoute = Ember.Route.extend({
-  model: function() {
-    return RECIPES;
-  }
-})
-
-App.IndexController = Ember.Controller.extend({
-  recipesCount: 5,
-    time: function() {
-    return (new Date()).toDateString();
-  }.property()
-});
-
-
-//<img {{bind-attr src='logo'}} alt='Logo' />
+var RECIPES = [{
+  name: 'banana',
+  description: 'this is a banana',
+  ingredients: 'banana',
+  directions: 'have a banana'
+}]
