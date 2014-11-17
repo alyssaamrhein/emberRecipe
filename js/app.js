a7fc12c213e02667ee65887dedb3403774a367b7
Alyssa <alyssa@thepalesnail.com>
2014-11-17 18:34:47 -0500

    working

--- js/app.js
+++ js/app.js
@@ -1,18 +1,55 @@
 App = Ember.Application.create();
 
+App.ApplicationSerializer = DS.LSSerializer.extend();
+App.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace: 'emberRecipe'
+});
+
+App.Recipe = DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  ingredients: DS.attr('string'),
+  directions: DS.attr('string')
+});
+
 App.Router.map(function() {
-  // put your routes here
+  this.route('recipes');
+  this.route('recipe', {path:'/recipes/:id' });
+  this.route('edit_recipe', {path:'/recipes/:id/edit'});
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return RECIPES;
+    return this.store.find("recipe");
   }
 });
 
-var RECIPES = [{
-  name: 'banana',
-  description: 'this is a banana',
-  ingredients: 'banana',
-  directions: 'have a banana'
-}]
+App.RecipeRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find("recipe", params.id);
+  }
+});
+
+App.EditRecipeRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find("recipe", params.id);
+  },
+  actions: {
+    updateRecipe: function(model, attributes){
+      model.setProperties(attributes);
+      model.save();
+      this.transitionTo('recipe', model);
+      return false;
+    }
+  }
+});
+
+App.EditRecipeController = Ember.ObjectController.extend({
+  actions: {
+     editFormSubmit : function(){
+      var attributes = this.getProperties('name', 'description','ingredients', 'directions');
+      this.send('updateRecipe', this.model, attributes);
+      return false;
+    }
+  }
+});
