688f71071ac867b91a77c08d063e8a150b783097
Alyssa <alyssa@thepalesnail.com>
2014-11-18 14:49:41 -0500

    things are working

--- js/models/recipemodel.js
+++ js/models/recipemodel.js
@@ -1,5 +1,6 @@
 App.Recipe = DS.Model.extend({
   name: DS.attr('string'),
+  imgUrl: DS.attr('string'),
   description: DS.attr('string'),
   ingredients: DS.attr('string'),
   directions: DS.attr('string')
