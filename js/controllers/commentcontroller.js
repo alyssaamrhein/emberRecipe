d00efbe902740de88edbc87e0a175c0357d393ea
Alyssa <alyssa@thepalesnail.com>
2014-11-20 21:04:26 -0500

    added comment things not working

---
+++ js/controllers/commentcontroller.js
@@ -0,0 +1,24 @@
+App.CommentController = Ember.ObjectController.extend({
+    markedText: function () {
+        return marked(this.get('text') || '');
+    }.property('text'),
+
+    needs: ['recipe'],
+    actions : {
+        save: function () {
+            if (!this.get('text')) { return; }//Do Nothing if text field is blank;
+            var comment = this.store.createRecord('comment', {
+                commenter: this.get('commenter'),
+                date: (new Date()).toDateString(),
+                text: this.get('text')
+            });
+            comment.save();
+
+            var recipe = this.get('controllers.recipe.model');
+            recipe.get('comments').pushObject(comment);
+            recipe.save();
+
+            this.transitionToRoute('recipe', recipe.id);
+        }
+    }
+});
--- js/router.js
+++ js/router.js
@@ -1,6 +1,8 @@
 App.Router.map(function() {
   this.route('recipes');
-  this.route('recipe', {path:'/recipes/:id' });
+  this.route('recipe', {path:'/recipes/:id' }, function (){
+    this.route('new_comment');
+  });
   this.route('edit_recipe', {path:'/recipes/:id/edit'});
   this.route('add_recipe', {path:'/recipes/new'});
   this.route('login', {path:'/login'});
